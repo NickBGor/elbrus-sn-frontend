0b60be7b9f25f1373e9c5edc817f4338f9344772
Katrin <alimova.katrin8@gmail.com>
2021-03-26 15:35:34 +0300

    email guard, confirmation page

--- src/pages/confirmation-mail-page.vue
+++ src/pages/confirmation-mail-page.vue
@@ -6,12 +6,21 @@
 
 <template>
   <div class="card-default confirm">
-    <p>Подтверди почту</p>
+    <p>{{ user.name }} , подтверди пожалуйста почту! 🚀</p>
+    <p>Мы выслали тебе письмо, так что проверь входящие</p>
+    PS: <small>и загляни на всякий случай в спам</small>
   </div>
 </template>
 
 <script>
+import store from '@/store';
+
 export default {
   name: 'confirmation-page',
+  data() {
+    return {
+      user: store.getters.getUser,
+    };
+  },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -21,48 +21,48 @@ const routes = [
     path: '/',
     name: 'news',
     component: NewsPage,
-    meta: { auth: true },
+    meta: { auth: true, isConfirmed: true },
   },
   {
     path: '/profile',
     name: 'profile',
-    meta: { auth: true },
+    meta: { auth: true, isConfirmed: true },
     component: ProfilePage,
   },
   {
     path: '/employers',
     name: 'employers',
-    meta: { auth: true },
+    meta: { auth: true, isConfirmed: true },
     component: EmployersPage,
   },
   {
     path: '/employers/:id',
     name: 'employer',
-    meta: { auth: true },
+    meta: { auth: true, isConfirmed: true },
     component: EmployerPage,
   },
   {
     path: '/ratings',
     name: 'ratings',
-    meta: { auth: true },
+    meta: { auth: true, isConfirmed: true },
     component: RatingPage,
   },
   {
     path: '/shop',
     name: 'shop',
-    meta: { auth: true },
+    meta: { auth: true, isConfirmed: true },
     component: ShopPage,
   },
   {
     path: '/mentor',
     name: 'mentor',
-    meta: { auth: true },
+    meta: { auth: true, isConfirmed: true },
     component: MentorPage,
   },
   {
     path: '/order-list',
     name: 'order-list',
-    meta: { auth: true },
+    meta: { auth: true, isConfirmed: true },
     component: OrderListPage,
   },
   {
@@ -81,7 +81,7 @@ const routes = [
     path: '/confirmation',
     name: 'confirmation',
     component: ConfirmPage,
-    meta: { auth: true },
+    meta: { auth: true, isConfirmed: false },
   },
   {
     path: '*',
@@ -99,15 +99,14 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   const requireAuthRoute = to.matched.some((e) => e.meta.auth);
+  const requireEmailRoute = to.matched.some((e) => e.meta.isConfirmed);
   const isAuth = store.getters.getAuth;
   const isConfirmedEmail = store.getters.getEmailStatus;
 
-  if (requireAuthRoute && isAuth && !isConfirmedEmail) {
-    console.log('isAuth && !isConfirmedEmail',isAuth && !isConfirmedEmail);
-    // next( { name: 'confirmation'})
-  }
   if (requireAuthRoute && !isAuth) {
     next({ name: 'login' });
+  } else if (requireEmailRoute && !isConfirmedEmail) {
+    next({ name: 'confirmation' });
   } else {
     next();
   }
