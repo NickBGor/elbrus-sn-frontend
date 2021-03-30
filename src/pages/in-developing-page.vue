9244ed76e57db9b2f26303dc232797f269f6076b
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2021-03-30 10:51:45 +0500

    add dev page

---
+++ src/pages/in-developing-page.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="flex justify-center flex-col items-center my-20 text-mainColor dark:text-darkModeTextColor"
+  >
+    <h1 class="text-4xl mb-4">
+      Функционал данной страницы находится в разработке.
+    </h1>
+    <h2 class="text-2xl mb-4">
+      Если у Вас есть желание поучаствовать в разработке проекта напишите вот
+      этому человека.
+    </h2>
+    <div>
+      <span class="material-icons">
+        arrow_downward
+      </span>
+      <span class="material-icons">
+        arrow_downward
+      </span>
+      <span class="material-icons">
+        arrow_downward
+      </span>
+    </div>
+    <a
+      href="https://t.me/nikitagorchakov"
+      target="_blank"
+      class="text-xl text-secondColor-100 hover:text-secondColor dark:text-thirdColor-100 dark:hover:text-thirdColor"
+    >
+      Никита
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'in-developing-page',
+};
+</script>
